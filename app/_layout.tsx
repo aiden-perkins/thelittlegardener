5bf06ed7a2fa75d10d744d88fcf8804d0bc8ee31
aiden-perkins <perkinsaidenm@gmail.com>
2025-04-27 04:24:26 -0700

    fix previous MyPlant showing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout() {
             tabBarShowLabel: false,
             headerShown: false,
           }}
+          getId={({ params }) => `plant-${params?.plantName}-${params?.userId}`}
         />
         <Tabs.Screen
             name="search/Search"
